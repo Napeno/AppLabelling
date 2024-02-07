91fba6261ac003d4ed0d829afb57195b3f94af25
UIT-21522029 <96015169+Napeno@users.noreply.github.com>
2024-02-08 01:43:48 +0700

    Thêm liên kết app với file Json

--- main.py
+++ main.py
@@ -5,22 +5,36 @@ from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
 import sys
 import os
+import json
 
 class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
         loadUi("main.ui", self)
+
+        # Diretory
         self.fname = ""
         self.directory = ""
         self.file_path = []
+        self.json_path = []
         self.count = 0
+
+        # Shapes inside Json data
+        self.shapes = []
+        self.label = []
+        self.text = []
+        self.points = []
+        self.group_id = []
+        self.shape_type = []
+        self.flags = []
+
         self.actionOpen_File.triggered.connect(self.openFile)
         self.actionOpen_Folder.triggered.connect(self.openFolder)
         self.actionOpen_Recent.triggered.connect(self.openRecent)
         self.actionSave.triggered.connect(self.Save)
         self.actionSave_as.triggered.connect(self.saveAs)
         self.actionSave_Automatically.triggered.connect(self.saveAuto)
-        self.actionChange_output_folder.triggered.connect(self.changeOutput)
+        self.actionChange_output_folder.triggered.connect(self.openJsonFiles)
         self.actionNext_Image.triggered.connect(self.nextImage)
         self.actionBack_Image.triggered.connect(self.backImage)
         self.actionClose.triggered.connect(self.Close)
@@ -36,13 +50,34 @@ class Main(QMainWindow):
 
     def openFolder(self):
         self.directory = QFileDialog.getExistingDirectory(self, 'Open Folder')
-        print(self.directory)
         for filename in os.listdir(self.directory):
             if(filename.endswith('.jpg') or filename.endswith('.png')):
                 self.file_path.append(os.path.join(self.directory, filename))
         self.showPic(self.file_path[0])
 
-        
+    def saveJsonData(self, data):
+        for self.shapes in data['shapes']:
+            self.label.append(self.shapes['label'])
+            self.text.append(self.shapes['text'])
+            self.points.append(self.shapes['points'])
+            self.group_id.append(self.shapes['group_id'])
+            self.shape_type.append(self.shapes['shape_type'])
+            self.flags.append(self.shapes['flags'])
+
+    def openJsonFiles(self):
+        self.directory = QFileDialog.getExistingDirectory(self, 'Open Folder')
+        for filename in os.listdir(self.directory):
+            if(filename.endswith('.json')):
+                self.json_path.append(os.path.join(self.directory, filename))
+
+        f = open(self.json_path[0])
+        data = json.load(f)
+
+        self.saveJsonData(data)
+
+        # print(self.points[0])
+
+        f.close()
 
     def openRecent(self):
         print('Open Recent')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+  # for shape in shapes:
+        #     label = shape['label']
+        #     text = shape['text']
+        #     points = shape['points']
